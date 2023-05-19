25d753d2cc0fb2de9fdfa567a05a53d841f82bc9
Mark Parnell <mark@sparkweb.com.au>
2023-05-19 11:41:33 +1000

    Further namespace adjustments to fix fatal errors when error response received from API

--- includes/vendor/guzzlehttp/guzzle/src/Exception/InvalidArgumentException.php
+++ includes/vendor/guzzlehttp/guzzle/src/Exception/InvalidArgumentException.php
@@ -2,6 +2,6 @@
 
 namespace Spark\Connexions\MailChimp\Vendor\GuzzleHttp\Exception;
 
-final class InvalidArgumentException extends \InvalidArgumentException implements \Spark\Connexions\MailChimp\Vendor\GuzzleException
+final class InvalidArgumentException extends \InvalidArgumentException implements GuzzleException
 {
 }
--- includes/vendor/guzzlehttp/guzzle/src/Exception/TransferException.php
+++ includes/vendor/guzzlehttp/guzzle/src/Exception/TransferException.php
@@ -2,6 +2,6 @@
 
 namespace Spark\Connexions\MailChimp\Vendor\GuzzleHttp\Exception;
 
-class TransferException extends \RuntimeException implements \Spark\Connexions\MailChimp\Vendor\GuzzleException
+class TransferException extends \RuntimeException implements GuzzleException
 {
 }
